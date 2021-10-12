2c47a2377b7eb2268e9b57dd82c4636fcc18e307
Andres Liu <andres.liu22@gmail.com>
2021-10-11 19:01:40 -0700

    Get repos from github

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("New updates coming soon!")
+$(document).ready(function (){
+    getRepos();
+});
+
+function getRepos() {
+    var apiUrl = 'https://api.github.com/users/andresliu22/repos?type=public&sort=created';
+  
+    fetch(apiUrl)
+      .then(function (response) {
+        if (response.ok) {
+          response.json().then(function (data) {
+            displayFeaturedRepos(data);
+          });
+        } else {
+          alert('Error: ' + response.statusText);
+        }
+      })
+      .catch(function (error) {
+        alert('Unable to connect to GitHub');
+      });
+  };
+
+ 
